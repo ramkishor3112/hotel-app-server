589c157dc09f4d3c6bd1fe74aaee13c086c2eeec
ramkishor3112 <153370093+ramkishor3112@users.noreply.github.com>
2023-12-09 18:53:50 +0530

    Add files via upload

---
+++ controllers/authController.js
@@ -0,0 +1,180 @@
+const bcrypt = require("bcryptjs");
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/userModel");
+const blacklistTokenData = require("../models/blacklist");
+const OTPModel = require("../models/otpModel");
+
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET;
+
+
+async function generateAndStoreOTP(email) {
+  try {
+    // Generate a random 6-digit OTP
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+    // Store the OTP in the database, associating it with the user
+    await OTPModel.create({
+      email,
+      otp,
+      createdAt: new Date(),
+    });
+
+    return otp;
+  } catch (error) {
+    console.log("Error Happened Inside generateAndStoreOTP", error);
+  }
+}
+async function verifyOTP(email, enteredOTP) {
+  try {
+    const OTP_EXPIRY_DURATION = 10 * 60 * 1000;
+    // Retrieve the stored OTP for the email from the database
+    const otpRecord = await OTPModel.findOne({
+      email,
+      otp: enteredOTP,
+    });
+
+    // Check if OTP is valid and not expired
+    if (
+      otpRecord &&
+      otpRecord.createdAt > new Date(Date.now() - OTP_EXPIRY_DURATION)
+    ) {
+      //  delete the OTP record from the database after successful verification
+      // await OTPModel.deleteOne({ otp: otpRecord.otp });
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.error("Error in OTP verification:", error);
+    // throw new Error("Error in OTP verification");
+  }
+}
+
+function generateToken(user) {
+  let payload = {
+    _id: user._id,
+    email: user.email,
+    name: user.name,
+  };
+  // console.log(payload);
+  return jwt.sign(payload, JWT_SECRET);
+}
+
+function verifyToken(token) {
+  const payload = jwt.verify(token, JWT_SECRET);
+  return payload;
+}
+
+async function signup(name, email, password) {
+  try {
+    const alreadyExisting = await User.findOne({ email });
+
+    if (alreadyExisting) {
+      return {
+        success: false,
+        message: "User already exists",
+      };
+    }
+
+    let user = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password),
+    });
+
+    user = user.toJSON();
+    delete user.password;
+
+    return {
+      success: true,
+      user,
+      message: "User registration successful",
+    };
+  } catch (error) {
+    console.error("Error in signup:", error);
+    return {
+      success: false,
+      message: "Something went wrong during signup",
+    };
+  }
+}
+
+async function login(email, password) {
+  let user = await User.findOne({
+    email,
+  });
+  if (user) {
+    user = user.toJSON();
+    if (bcrypt.compareSync(password, user.password)) {
+      delete user.password;
+      return {
+        data: {
+          token: generateToken(user),
+          user,
+        },
+      };
+    } else {
+      throw new Error("password does not match");
+    }
+  } else {
+    throw new Error("User Already Present");
+  }
+}
+
+async function getUserById(id) {
+  let user = await User.findById(id);
+
+  user = user.toJSON();
+  delete user.password;
+  return user;
+}
+
+async function addTokenToBlacklist(token) {
+  let blacklisted = await blacklistTokenData.create({ token });
+
+  return blacklisted;
+}
+
+async function resetPassword(email, newPassword) {
+  try {
+    // Find the user by email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      // customized the response based on  frontend needs
+      return {
+        success: false,
+        message: "User not found",
+      };
+    }
+
+    // Update the user's password
+    // password: bcrypt.hashSync(password),
+    user.password = bcrypt.hashSync(newPassword);
+  //  console.log("UpdatedUser",user)
+    // Save the updated user object
+    await user.save();
+
+    return {
+      success: true,
+      message: "Password reset successful",
+    };
+  } catch (error) {
+    console.error("Error in password reset:", error);
+    throw new Error("Error in password reset");
+  }
+}
+
+module.exports = {
+  signup,
+  login,
+  verifyOTP,
+  verifyToken,
+  getUserById,
+  addTokenToBlacklist,
+  generateAndStoreOTP,
+  resetPassword,
+};
---
+++ routes/auth.routes.js
@@ -0,0 +1,313 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const nodemailer = require("nodemailer");
+const {
+  signup,
+  login,
+  addTokenToBlacklist,
+  verifyOTP,
+  generateAndStoreOTP,
+  resetPassword,
+} = require("../controllers/authController");
+const User = require("../models/userModel");
+const city = require("../models/City");
+const country = require("../models/country");
+const hotelRecord = require("../models/hotelRecord");
+
+dotenv.config();
+
+const authRouter = express.Router();
+
+authRouter.get("/", async (req, res) => {
+  try {
+    return res.status(201).send({
+      message: "Hello From Backend",
+    });
+  } catch (err) {
+    return res.status(400).send({
+      error: "Error Happened in Backend",
+    });
+  }
+});
+
+authRouter.get("/getData", async (req, res) => {
+  try {
+    const pipeline = [
+      {
+        $lookup: {
+          from: "cities",
+          localField: "hotelCode",
+          foreignField: "hotelCode",
+          as: "cityData",
+        },
+      },
+      {
+        $lookup: {
+          from: "countries",
+          localField: "hotelCode",
+          foreignField: "hotelCode",
+          as: "countryData",
+        },
+      },
+      {
+        $unwind: {
+          path: "$cityData",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $unwind: {
+          path: "$countryData",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          hotelCode: 1,
+          hotelName: 1,
+          hotelCountry: "$countryData.hotelCountry",
+          countryCode: "$countryData.countryCode",
+          hotelCity: "$cityData.hotelCity",
+          cityCode: "$cityData.cityCode",
+          created_at: 1,
+          __v: 1,
+        },
+      },
+    ];
+
+    const aggregatedData = await hotelRecord.aggregate(pipeline).exec();
+
+    return res.status(201).send(aggregatedData);
+  } catch (err) {
+    return res.status(400).send({
+      error: "Error happened in the backend",
+    });
+  }
+});
+
+authRouter.post("/addHotelData", async (req, res) => {
+  try {
+    const { hotelName, hotelCity, cityCode, hotelCountry, countryCode } =
+      req.body;
+
+    // Find the last hotel record to generate a unique hotelCode
+    const lastHotel = await hotelRecord.findOne(
+      {},
+      {},
+      { sort: { created_at: -1 } }
+    );
+    const lastNumber = lastHotel
+      ? parseInt(lastHotel.hotelCode.match(/\d+/)[0])
+      : 0;
+    const hotelCode = `EL${lastNumber + 1}`;
+
+    // Creating a new City document and saving it to the database
+    const newCity = await city.create({
+      hotelCode,
+      cityCode,
+      hotelCity,
+    });
+
+    // Creating a new Hotel document and saving it to the database
+    const newHotel = await hotelRecord.create({
+      hotelCode,
+      hotelName,
+      cityCode,
+      countryCode,
+    });
+
+    // Creating a new Country document and saving it to the database
+    const newCountry = await country.create({
+      hotelCode,
+      countryCode,
+      hotelCountry,
+    });
+
+    return res.status(201).send({
+      message: "Data added to DB",
+      data: { newHotel, newCity, newCountry },
+    });
+  } catch (err) {
+    console.error("Error adding hotel data:", err);
+    return res.status(500).send({
+      error: "Error adding hotel data to the database",
+    });
+  }
+});
+
+authRouter.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    let user = await signup(name, email, password);
+
+    return res.status(201).send({
+      data: user,
+    });
+  } catch (err) {
+    return res.status(400).send({
+      error: "Something went wrong",
+    });
+  }
+});
+
+authRouter.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const data = await login(email, password);
+
+    return res.send({
+      message: "Login Successfull",
+      data,
+    });
+  } catch (err) {
+    let errorMessage =
+      "Something went wrong, Either Email or password is Wrong" +
+      "  " +
+      err.message;
+
+    if (err.message === "password does not match") {
+      errorMessage = "Password Does Not Match";
+    } else if (err.message === "User Already Present") {
+      errorMessage = "User with this email Does Not Exist";
+    }
+
+    return res.status(401).send({
+      error: errorMessage,
+    });
+  }
+});
+
+//  /forgot-password route
+authRouter.post("/forgot-password", async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    // Check if the email exists in the database
+    const user = await User.findOne({ email });
+    if (!user) {
+      console.log("User with this email does not exist");
+      return res.status(404).send({
+        error: "User with this email does not exist",
+      });
+    }
+
+    // Generate and store OTP
+    const otp = await generateAndStoreOTP(email);
+
+    // Send the OTP to the user's email
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "singhsuryaprakash110@gmail.com",
+        pass: process.env.GOOGLE_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: "singhsuryaprakash110@gmail.com",
+      to: email,
+      subject: "Password Reset OTP",
+      text: `Your OTP for password reset is: ${otp}`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email:", error);
+        return res.status(500).send({
+          error: "Failed to send OTP via email",
+        });
+      }
+
+      console.log("Email sent:", info.response);
+      return res.status(200).send({
+        message: "OTP sent successfully",
+        email: user.email, // Send the email as a response
+      });
+    });
+  } catch (err) {
+    console.error("Inside catch part of forgot-password:", err);
+    return res.status(500).send({
+      error: "Something went wrong",
+    });
+  }
+});
+
+authRouter.post("/verify-otp", async (req, res) => {
+  try {
+    const { email, otp } = req.body;
+
+    // Call a function to verify the OTP
+    const isOTPValid = await verifyOTP(email, otp);
+
+    if (isOTPValid) {
+      return res.status(200).send({
+        message: "OTP verified successfully",
+        email: email, // Send the email as a response
+      });
+    } else {
+      return res.status(401).send({
+        error: "Invalid OTP",
+      });
+    }
+  } catch (err) {
+    console.error("Error in OTP verification:", err);
+    return res.status(500).send({
+      error: "Something went wrong",
+    });
+  }
+});
+
+//  Reset password route
+authRouter.post("/reset-password", async (req, res) => {
+  try {
+    const { email, otp, newPassword } = req.body;
+
+    // Verify OTP
+    const isOTPValid = await verifyOTP(email, otp);
+
+    if (!isOTPValid) {
+      return res.status(401).send({
+        error: "Invalid OTP",
+      });
+    }
+
+    // Reset password
+    const resetResult = await resetPassword(email, newPassword);
+
+    if (!resetResult.success) {
+      return res.status(404).send({
+        error: resetResult.message,
+      });
+    }
+
+    return res.status(200).send({
+      message: resetResult.message,
+    });
+  } catch (err) {
+    console.error("Error in reset password:", err);
+    return res.status(500).send({
+      error: "Something went wrong",
+    });
+  }
+});
+
+authRouter.post("/logout", async (req, res) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    const data = await addTokenToBlacklist(token);
+
+    return res.send({
+      message: "Logout Successful",
+      data,
+    });
+  } catch (err) {
+    return res.status(500).send({
+      error: "Something went wrong",
+    });
+  }
+});
+
+module.exports = authRouter;
